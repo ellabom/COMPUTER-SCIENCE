789999ce86956f4829edb98d4cc750ba3e65def9
Emmanuella Eyo <64244207+ellabom@users.noreply.github.com>
2023-12-27 15:47:30 -0600

    Add files via upload

---
+++ INTRODUCTION TO AI/A10/simple_testing.py
@@ -0,0 +1,18 @@
+import DecisionTree as DT
+import sys
+
+if len(sys.argv) < 2:
+    print("Usage python simple_testing.py <filename>")
+    exit()
+    
+filename = sys.argv[1]
+
+mytree = DT.Decision_Tree()
+
+mytree.build(filename)
+print(mytree)
+print("Tree size:", mytree.size())
+
+#d = {"x":"t", "y":"f"}
+#print()
+#print(mytree.classify(d))
